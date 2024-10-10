bf4b4ba1dd5081f13629ea7924ebba54b08702df
ygo1139 <ngn2349568@stu.o-hara.ac.jp>
2024-10-10 14:32:39 +0900

    a

--- src/bean/Product.java
+++ src/bean/Product.java
@@ -1,5 +1,19 @@
 package bean;
 
-public class Product {
-
+public class Product implements java.io.Serializable{
+	
+	private int id;
+	private String name;
+	private int price;
+	
+	public int getId(){
+		return id;
+		
+	}
+	public String getName(){
+		return name;
+	}
+	public int getPrice(){
+		return price;
+	}
 }
