56ca3c998a4a9166604e4fb868e7769ccc506a4a
djangouser <user@wxample.com>
2024-10-10 14:12:13 +0900

    oaoaoa

--- src/bean/User.java
+++ src/bean/User.java
@@ -1,5 +1,72 @@
 package bean;
 
-public class User {
+public class User implements java.io.Serializable{
+	private String user_id;
+	private String maleaddress;
+	private String password;
+	private String user_name;
+	private	 String user_hurigana;
+	private String user_address;
+	private String telephone;
+
+	public String getUser_id(){
+		return user_id;
+	}
+
+	public String getMaleaddress(){
+		return maleaddress;
+	}
+
+	public String getPassword(){
+		return password;
+	}
+
+	public String getUser_name(){
+		return user_name;
+	}
+
+	public String getUser_hurigana(){
+		return user_hurigana;
+	}
+
+	public String getUser_address(){
+		return user_address;
+	}
+
+	public String getTelephone(){
+		return telephone;
+	}
+
+
+	public void setUser_id(String user_id) {
+		this.user_id=user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id=user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id=user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id=user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id=user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id=user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id=user_id;
+	}
+
+
+
 
 }
