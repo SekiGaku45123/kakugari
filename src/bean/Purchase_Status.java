5ffb9e1464c89272a4be5dde1faddca1cf8e4d82
ygo1139 <ngn2349568@stu.o-hara.ac.jp>
2024-10-10 14:31:59 +0900

    a

---
+++ src/bean/Purchase_Status.java
@@ -0,0 +1,16 @@
+package bean;
+
+public class Purchase_Status implements java.io.Serializable {
+	private int flag_id;
+    private String condition;
+
+
+	public int getFlag_id() {
+        return flag_id;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+}
