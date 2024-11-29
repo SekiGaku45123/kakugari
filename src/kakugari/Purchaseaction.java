c0a1e9d7d7265589e724f33f8910926d51b051fb
djangouser <user@wxample.com>
2024-11-29 11:47:08 +0900

    knj

--- src/kakugari/Purchaseaction.java
+++ src/kakugari/Purchaseaction.java
@@ -12,8 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import bean.History;
+import bean.Item;
+import bean.Transaction;
 import bean.User;
 import dao.BuysearchDAO;
+import dao.ItemDAO;
+import dao.TransactionDAO;
 
 @WebServlet(urlPatterns={"/main_kakugari/Purchaseaction"})
 public class Purchaseaction extends HttpServlet {
@@ -39,6 +43,7 @@ public class Purchaseaction extends HttpServlet {
 
         // 購入情報をリクエストから取得
         String itemIdStr = request.getParameter("item_id");
+        String user_id_id = request.getParameter("user_id_id");
         System.out.print(itemIdStr);
         int item_id = Integer.parseInt(itemIdStr.trim());
         System.out.print(item_id);
@@ -75,6 +80,31 @@ public class Purchaseaction extends HttpServlet {
 
             // 挿入が成功した場合、購入完了ページへ遷移
             if (result > 0) {
+
+            	String s_item_id = String.valueOf(item_id);
+
+            	Item item = new Item();
+            	item.setItem_id(s_item_id);
+
+            	ItemDAO dao1 = new ItemDAO();
+            	int line = dao1.updateinsert(item);
+
+            	String item_id_id = String.valueOf(item_id);
+
+            	Transaction transaction = new Transaction();
+            	transaction.setItem_id(item_id_id);
+            	transaction.setPurchaser(user_id);
+            	transaction.setExhibit_user(user_id_id);
+
+            	
+            	
+            	TransactionDAO dao2 = new TransactionDAO();
+            	int line_second = dao2.insert(transaction);
+
+            	System.out.print(line_second);
+
+            	System.out.print(line);
+
                 System.out.println("購入処理が完了しました。");
                 session.removeAttribute("cart");
                 request.getRequestDispatcher("purchase-out.jsp").forward(request, response);
